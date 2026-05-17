355377337a21a925a1749130e8c463194376cc39
angel <angel_guerrero1@ucne.edu.do>
2026-05-17 00:23:15 -0400

    Convierte la fecha en string

---
+++ app/src/main/java/com/android/registroocupaciones/data/empleado/local/Converters.kt
@@ -0,0 +1,17 @@
+package com.android.registroocupaciones.data.empleado.local
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromString(value:String?):LocalDate?{
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun toString(date: LocalDate?): String?
+    {
+        return date?.toString()
+    }
+}
